a71eadd8fb655a23e4b469442a0f310c94fadc76
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-12 10:10:56 +0200

    EG - fixed insert() when parent or newnode is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,10 +14,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->left = parent->left;
 	if (new_node->left != NULL)
 		new_node->left->parent = new_node;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,10 +14,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->right = parent->right;
 	if (new_node->right != NULL)
 		new_node->right->parent = new_node;
