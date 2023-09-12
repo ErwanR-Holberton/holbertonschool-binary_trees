e511c0f796dc8d43a8206a6e6e4193726e849251
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-12 11:05:21 +0200

    EG - simplified size()

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,13 +6,7 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	int size_left = 0, size_right = 0;
-
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
-		size_left = binary_tree_size(tree->left);
-	if (tree->right != NULL)
-		size_right = binary_tree_size(tree->right);
-	return (size_left + size_right + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
