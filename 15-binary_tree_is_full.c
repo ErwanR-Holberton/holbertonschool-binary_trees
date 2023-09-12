6966df6ba5a2afce6a67613521f7cef028128164
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-12 14:51:54 +0200

    EG - fixed function is_full()

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,7 +1,6 @@
 #include "binary_trees.h"
 #include "11-binary_tree_size.c"
-#include "4-binary_tree_is_leaf.c"
-#include <stdio.h>
+#include "9-binary_tree_height.c"
 /**
  * binary_tree_is_full - checks if tree is full
  *@tree: tree to check
@@ -10,12 +9,19 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	size_t size, i, j, sum = 0, pow;
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_is_leaf(tree))
+	size = binary_tree_size(tree);
+
+	for (i = 0; i <= binary_tree_height(tree); i++)
+	{
+		for (j = 0, pow = 1; j < i; j++)
+			pow *= 2;
+		sum += pow;
+	}
+	if (sum == size)
 		return (1);
-	if (binary_tree_size(tree->left) != binary_tree_size(tree->right))
-		return (0);
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
