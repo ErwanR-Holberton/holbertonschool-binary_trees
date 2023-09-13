6350337ec61e28221d68373a572eaf8a043a1953
ErwanR-Holberton <6218@holbertonstudents.com>
2023-09-13 13:48:44 +0200

    Edited description for function is_complete()

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -4,15 +4,17 @@
 /**
  * binary_tree_is_complete - check if the given tree is complete
  * @tree: tree to loop through
- * Return - 1 ifcomplete 0 otherwise
+ * Return: 1 if complete 0 otherwise
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	int balance;
+
 	if (tree == NULL)
 		return (0);
+
 	balance = binary_tree_balance(tree);
-	/*printf("balance de %d : %d\n", tree->n, balance);*/
+
 	if (balance < 0 || balance > 1)
 		return (0);
 	if (tree->left == NULL || tree->right == NULL)
@@ -20,11 +22,6 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (balance == 0)
 		if (binary_tree_is_full(tree->left) == 0)
 			return (0);
-	return (binary_tree_is_complete(tree->left) && binary_tree_is_complete(tree->right));
-
-	/*if (binary_tree_balance(tree->left) < 0)
-		return (0);
-	if (binary_tree_balance(tree->right) < 0)
-		return(0);
-	return(1);*/
+	return (binary_tree_is_complete(tree->left) &&
+	binary_tree_is_complete(tree->right));
 }
